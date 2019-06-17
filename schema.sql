480ba3f676cc673280c38b4967a6a582303cad92
Elliot Foster & Ben Kovacevich <efoster@shutterstock.com>
2019-06-17 15:33:03 -0500

    Update schema to match production

--- schema.sql
+++ schema.sql
@@ -8,7 +8,5 @@ CREATE TABLE jobs (
   create_time  timestamp not null default '2001-01-01 00:00:00',
   update_time  timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
 
-  KEY          ix_queue_locked_until (queue,locked_until),
-  KEY          ix_locked_until (locked_until),
   KEY          ix_worker (worker)
 );
